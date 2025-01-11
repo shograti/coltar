fa366221fac08374b98190390f5fba486c45d31c
shograti <petitberghienleo@gmail.com>
2025-01-11 14:36:55 +0100

    Styled posts

--- src/app/components/post/post.tsx
+++ src/app/components/post/post.tsx
@@ -5,14 +5,13 @@ import { Post as PostType } from "payload-types";
 
 export function Post({ post }: { post: PostType }) {
   return (
-    <article>
+    <article className={styles.post}>
       <Link href={`/poemes/${post.slug}`}>
         <h2 className={styles.title}>{post.title}</h2>
       </Link>
       <RichText className={styles.text_block} data={post.content} />
       <p className={styles.authors}>
-        Auteur :{" "}
-        <Link href={`/auteurs/${post.author}`}> {post.author}</Link>
+        Auteur : <Link href={`/auteurs/${post.author}`}> {post.author}</Link>
       </p>
     </article>
   );
